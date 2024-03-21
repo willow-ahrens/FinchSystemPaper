13619d7c230d66050b3e819df43eeae249fe4d4f
Willow Ahrens <willow.marie.ahrens@gmail.com>
2024-03-21 13:17:46 -0400

    fix

---
+++ export_looplet_figures.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+file=Looplets # File name excluding extension
+
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 0 --output "$file-lookup.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 1 --output "$file-run.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 2 --output "$file-spike.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 3 --output "$file-sequence.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 4 --output "$file-stepper.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 5 --output "$file-phase.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 6 --output "$file-switch.png" "$file.drawio"
+/Applications/draw.io.app/Contents/MacOS/draw.io --export --page-index 7 --output "$file-thunk.png" "$file.drawio"
